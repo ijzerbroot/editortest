a5eea788ce3c012a3472e1504eeb1fe1322139e5
Frank Hoeben <f.hoeben@gmail.com>
2018-06-03 08:47:40 +0200

    Create javafile.java
    simple java class

---
+++ javafile.java
@@ -0,0 +1,8 @@
+public class Eclimtest {
+    public static void main(String... args) {
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(i);
+        }
+        System.out.println("All done!");
+    }
+}
